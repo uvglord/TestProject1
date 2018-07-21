ba70a0f7026bc1e56c5e5bfcb2dfc0b14b7857b0
saura <saura@192.168.0.107>
2018-07-21 17:28:33 +0530

    more bubble sort examples

--- src/main/java/com/uvglord/corejava/sorting/bubblesort/MyBubbleSort.java
+++ src/main/java/com/uvglord/corejava/sorting/bubblesort/MyBubbleSort.java
@@ -1,29 +1,27 @@
 package com.uvglord.corejava.sorting.bubblesort;
 
-import java.lang.reflect.Array;
-
 public class MyBubbleSort {
 
 	public static void main(String[] args) {
-		int[] input = {4, 2, 9, 6, 23, 12, 34, 0, 1};
+		int[] input = { 4, 2, 9, 6, 23, 12, 34, 0, 1 };
 		printNumbers(input);
 		bubbleSort(input);
 	}
 
-	//logic to sort the elements
+	// logic to sort the elements
 	private static void bubbleSort(int[] input) {
 		int n = input.length;
 		int k;
-		for(int m=n; m>=0; m--) {
-			for(int i=0; i<n-1; i++) {
-				k = i+1;
-				if(input[i]>input[k]) {
+		for (int m = n; m >= 0; m--) {
+			for (int i = 0; i < n - 1; i++) {
+				k = i + 1;
+				if (input[i] > input[k]) {
 					swapNumbers(i, k, input);
 				}
 			}
 			printNumbers(input);
 		}
-		
+
 	}
 
 	private static void swapNumbers(int i, int k, int[] input) {
@@ -32,12 +30,12 @@ public class MyBubbleSort {
 		input[i] = input[k];
 		input[k] = temp;
 	}
-	
+
 	private static void printNumbers(int[] input) {
-		for(int i=0; i<input.length; i++) {
-			System.out.print(input[i]+", ");
+		for (int i = 0; i < input.length; i++) {
+			System.out.print(input[i] + ", ");
 		}
 		System.out.println("\n");
-		
+
 	}
 }
